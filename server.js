8b49536263bc5d850f2d7f1c13e27cb35a04160f
jacksonneaves-tech <jackson.neaves@hotmail.com>
2026-02-20 21:15:15 +1100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ2UDZiSkhka1lhQ3lRV3BDSUp1WnVhdUpmdHNnellucUZqNDgzYU5ZMGhjIiwiaWF0IjoxNzcxNTc5MTIwfQ.KpzxhHRqxRNxH-0pNAQSSWNv05cRHkn-r6rdTF0ItYg";
 
 const GTFS_URL =
   "https://api.opendata.transport.vic.gov.au/opendata/public-transport/gtfs/realtime/v1/bus/vehicle-positions";
